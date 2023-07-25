1f09edb4ef3527972e92e51df53e81e49e09475e
YuriCha02 <115993852+YuriCha02@users.noreply.github.com>
2023-07-25 15:20:40 +0900

    1차 업데이트.py

--- MLPClassifier.py
+++ MLPClassifier.py
@@ -5,12 +5,16 @@ from imblearn.over_sampling import SMOTE
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report
 from sklearn.metrics import *
+import joblib
 
 def load_data():
     mc_data = pd.read_csv('./mulit_classification_data.csv')
-    faluts = (mc_data['Pastry'] + mc_data['Z_Scratch'] + mc_data['K_Scatch'] + mc_data['Stains'] 
-              + mc_data['Dirtiness'] + mc_data['Bumps'] + mc_data['Other_Faults'])
     combined_data = combine_labels(mc_data)
+    combined_data['Width'] = combined_data['X_Maximum'] - combined_data['X_Minimum']
+    combined_data['Length'] = combined_data['Y_Maximum'] - combined_data['Y_Minimum']
+    combined_data['Total_Perimeter'] = combined_data['X_Perimeter'] * combined_data['Y_Perimeter']
+    combined_data['Model_Difference'] = abs(combined_data['TypeOfSteel_A300'] - combined_data['TypeOfSteel_A400'])
+
     return combined_data
     
 def combine_labels(data):
@@ -35,16 +39,17 @@ def combine_labels(data):
     return pd.DataFrame(new_data, columns=new_columns) #pandas에 쓸수 있게 새로운 데이터를 데이터 프레임화 시킴.
 
 def split_standardize(combined_data):
-    X = combined_data.drop('Faults', axis = 1)
+    X = combined_data.drop(['Faults'], axis = 1)
     y = combined_data['Faults']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .2, stratify = y, random_state = 42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .3, stratify = y, random_state = 42)
 
     ss = StandardScaler()
+
 
     X_train_ss = ss.fit_transform(X_train)
     X_test_ss = ss.transform(X_test)
 
-    return X_train_ss, X_test_ss, y_train, y_test
+    return X_train_ss, X_test_ss, y_train, y_test, ss
 
 def upsampling(X_train_ss, y_train):
     X_train_sampled, y_train_sampled = SMOTE(random_state=42).fit_resample(
@@ -53,15 +58,15 @@ def upsampling(X_train_ss, y_train):
     return X_train_sampled, y_train_sampled
 
 clf = MLPClassifier(
-    hidden_layer_sizes=(50, 50),
+    hidden_layer_sizes=(110, 50),
     batch_size = 1,
-    max_iter=50,
-    alpha=0.0001,
+    max_iter=5000,
+    alpha=0.001,
     solver='adam',
     activation='relu',
     learning_rate_init = 0.001,
-    momentum = 0.9,
-    tol = 0.0001,
+    momentum = 0.8,
+    tol = 0.00001,
     early_stopping = True,
     validation_fraction = 0.1,
     random_state = 42,
@@ -70,15 +75,23 @@ clf = MLPClassifier(
 
 def train_data():
     combined_data = load_data()
-    X_train, X_test, y_train, y_test = split_standardize(combined_data)
+    X_train, X_test, y_train, y_test, ss = split_standardize(combined_data)
     X_train_sampled, y_train_sampled = upsampling(X_train, y_train)
     clf.fit(X_train_sampled, y_train_sampled)
     y_test_pred = clf.predict(X_test)
     accuracy = clf.score(X_test, y_test)
     print("Test Accuracy:", accuracy)
     print(classification_report(y_test, y_test_pred))
+    joblib.dump(clf, 'mlp_model.joblib')
+
+    return clf, ss
+
+clf, ss = train_data()
 
 if __name__ == "__main__":
     train_data()
 
+joblib.dump(clf, 'mlp_model.joblib')
+ds = joblib.load('mlp_model.joblib')
 
+ds.predict()
